1f8a8071d579f733314c9bc227b1b9686ddefa29
huynhdthanhtuan <tuan40655@gmail.com>
2021-12-21 00:42:14 +0700

    Fetch data by using axious: DONE, need rework for performance!

---
+++ src/apis/apis.js
@@ -0,0 +1,4 @@
+// The REST API endpoints
+export const API1_URL = "https://61bc99f0d8542f001782486b.mockapi.io/api/1";
+export const API2_URL = "https://61bc99f0d8542f001782486b.mockapi.io/api/2";
+export const API3_URL = "https://61bc99f0d8542f001782486b.mockapi.io/api/3";
--- src/apis/fetchData.js
+++ src/apis/fetchData.js
@@ -1,9 +1,5 @@
 import axios from "axios";
-
-// The REST API endpoints
-const API1_URL = "https://61bc99f0d8542f001782486b.mockapi.io/api/1";
-const API2_URL = "https://61bc99f0d8542f001782486b.mockapi.io/api/2";
-const API3_URL = "https://61bc99f0d8542f001782486b.mockapi.io/api/3";
+import { API1_URL, API2_URL, API3_URL } from "./apis";
 
 async function fetchData() {
   const { data: responedData1 } = await axios.get(API1_URL);
