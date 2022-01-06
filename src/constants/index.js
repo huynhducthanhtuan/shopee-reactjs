950de31b3216531b029ed51a10eae06ad253f6a2
huynhdthanhtuan <tuan40655@gmail.com>
2022-01-06 23:51:23 +0700

    handle logic for RegisterPage: to FirstForm

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,2 @@
 const $ = document.querySelector.bind(document);
 export const COUNT_DOWN_SECONDS = 7;
-
-// export const html = $("html");
-// export const body = $("body");
-// export const app = $("#app");
-// export const modal = $("#modal");
-// export const container = $("#container");
